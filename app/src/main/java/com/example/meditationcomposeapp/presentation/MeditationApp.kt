9a13b7afafeab53529390f413620372a529e62d5
Parocq <55479321+Parocq@users.noreply.github.com>
2022-04-02 12:59:16 +0400

    feat: added Hilt

--- app/src/main/java/com/example/meditationcomposeapp/presentation/MeditationApp.kt
+++ app/src/main/java/com/example/meditationcomposeapp/presentation/MeditationApp.kt
@@ -1,7 +1,9 @@
 package com.example.meditationcomposeapp.presentation
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MeditationApp: Application() {
 
     override fun onCreate() {
