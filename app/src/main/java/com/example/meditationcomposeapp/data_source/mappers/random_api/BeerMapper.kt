b1cd49ca31aea362542a8eefdd5eb8890c7faecc
AlekseyHerman <aliaksei.herman@fingers-media.com>
2022-09-16 22:39:11 +0300

    feat: update beer model; remove unused fields

--- app/src/main/java/com/example/meditationcomposeapp/data_source/mappers/random_api/BeerMapper.kt
+++ app/src/main/java/com/example/meditationcomposeapp/data_source/mappers/random_api/BeerMapper.kt
@@ -8,17 +8,13 @@ import javax.inject.Inject
 class BeerMapper @Inject constructor() : Mapper<Beer, BeerResponse> {
     override fun mapFrom(objectFrom: BeerResponse): Beer {
         return Beer(
-            id = objectFrom.id,
-            uid = objectFrom.uid,
-            brand = objectFrom.brand,
-            name = objectFrom.name,
-            style = objectFrom.style,
-            hop = objectFrom.hop,
-            yeast = objectFrom.yeast,
-            malts = objectFrom.malts,
-            ibu = objectFrom.ibu,
-            alcohol = objectFrom.alcohol,
-            blg = objectFrom.blg
+            brand = objectFrom.brand ?: "",
+            name = objectFrom.name ?: "",
+            style = objectFrom.style ?: "",
+            malts = objectFrom.malts ?: "",
+            ibu = objectFrom.ibu ?: "",
+            alcohol = objectFrom.alcohol ?: "",
+            blg = objectFrom.blg ?: ""
         )
     }
 }
--- app/src/main/java/com/example/meditationcomposeapp/model/entity/Beer.kt
+++ app/src/main/java/com/example/meditationcomposeapp/model/entity/Beer.kt
@@ -1,15 +1,11 @@
 package com.example.meditationcomposeapp.model.entity
 
 data class Beer(
-    var id: Int?,
-    var uid: String?,
-    var brand: String?,
-    var name: String?,
-    var style: String?,
-    var hop: String?,
-    var yeast: String?,
-    var malts: String?,
-    var ibu: String?,
-    var alcohol: String?,
-    var blg: String?
+    var brand: String,
+    var name: String,
+    var style: String,
+    var malts: String,
+    var ibu: String,
+    var alcohol: String,
+    var blg: String
 )
