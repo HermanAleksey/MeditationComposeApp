3befd1188ddc0175e003c0fdd1414a6c74d80da2
AlekseyHerman <aliaksei.herman@fingers-media.com>
2022-11-03 16:16:55 +0300

    feat: migrate settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven { url = 'https://jitpack.io' }
+        maven ( url = "https://jitpack.io" )
     }
 }
 dependencyResolutionManagement {
@@ -11,9 +11,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = 'https://jitpack.io' }
+        maven ( url = "https://jitpack.io" )
     }
 }
+
 rootProject.name = "MeditationComposeApp"
-include ':app'
-include ':shuffle_puzzle'
+include(":app", ":shuffle_puzzle", ":buildsrc")
