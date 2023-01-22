b98d84eb76bb836c5829a6f863143b8c66092f3f
AlekseyHerman <aliaksei.herman@fingers-media.com>
2023-01-22 16:43:30 +0300

    feat: refactor BeerPagingRemoteMediator.kt with use of new use-cases

--- app/src/main/java/com/example/meditationcomposeapp/model/usecase/punk/BeerPagingRemoteMediator.kt
+++ app/src/main/java/com/example/meditationcomposeapp/model/usecase/punk/BeerPagingRemoteMediator.kt
@@ -1,90 +1,116 @@
 package com.example.meditationcomposeapp.model.usecase.punk
 
-import android.util.Log
-import androidx.paging.ExperimentalPagingApi
-import androidx.paging.LoadType
-import androidx.paging.PagingState
-import androidx.paging.RemoteMediator
-import com.example.meditationcomposeapp.model.entity.beer.Beer
+import androidx.paging.*
+import com.example.meditationcomposeapp.data_source.entity.db.BeerListItem
+import com.example.meditationcomposeapp.data_source.entity.db.RemoteKeys
+import com.example.meditationcomposeapp.model.usecase.ExecuteDatabaseTransactionUseCase
+import com.example.meditationcomposeapp.model.usecase.punk.db.ClearBeersDBUseCase
+import com.example.meditationcomposeapp.model.usecase.punk.db.InsertBeersIntoDBUseCase
+import com.example.meditationcomposeapp.model.usecase.punk.network.GetBeersUseCase
+import com.example.meditationcomposeapp.model.usecase.remote_keys.*
 import retrofit2.HttpException
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 @OptIn(ExperimentalPagingApi::class)
 class BeerPagingRemoteMediator(
-    private val pageSize: Int,
-
     private val insertBeersIntoDBUseCase: InsertBeersIntoDBUseCase,
     private val clearBeersDBUseCase: ClearBeersDBUseCase,
+    private val getCreationTimeUseCase: GetCreationTimeUseCase,
+    private val clearRemoteKeysUseCase: ClearRemoteKeysUseCase,
+    private val getRemoteKeyBeBeerIdUseCase: GetRemoteKeyBeBeerIdUseCase,
+    private val insertAllRemoteKeysUseCase: InsertAllRemoteKeysUseCase,
+
     private val getBeersUseCase: GetBeersUseCase,
-) : RemoteMediator<Int, Beer>() {
+    private val executeDatabaseTransaction: ExecuteDatabaseTransactionUseCase,
+) : RemoteMediator<Int, BeerListItem>() {
 
     override suspend fun initialize(): InitializeAction {
         val cacheTimeout = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
-        return if (true/*System.currentTimeMillis() - db.lastUpdated() <= cacheTimeout*/) {
-            // Cached data is up-to-date, so there is no need to re-fetch
-            // from the network.
+        return if (System.currentTimeMillis() - (getCreationTimeUseCase() ?: 0) < cacheTimeout
+        ) {
             InitializeAction.SKIP_INITIAL_REFRESH
         } else {
-            // Need to refresh cached data from network; returning
-            // LAUNCH_INITIAL_REFRESH here will also block RemoteMediator's
-            // APPEND and PREPEND from running until REFRESH succeeds.
             InitializeAction.LAUNCH_INITIAL_REFRESH
         }
     }
 
     override suspend fun load(
         loadType: LoadType,
-        state: PagingState<Int, Beer>
+        state: PagingState<Int, BeerListItem>
     ): MediatorResult {
         return try {
-            val nextPageToLoad = when (loadType) {
+            val page: Int = when (loadType) {
                 LoadType.REFRESH -> {
-                    Log.e("TAGG", "load: REFRESH")
-                    null
+                    val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                    remoteKeys?.nextKey?.minus(1) ?: 1
                 }
                 LoadType.PREPEND -> {
-                    Log.e("TAGG", "load: PREPEND :${state.pages.last().nextKey}")
-//                    state.pages.last().nextKey
-                    return MediatorResult.Success(
-                        endOfPaginationReached = false
-                    )
+                    val remoteKeys = getRemoteKeyForFirstItem(state)
+                    val prevKey = remoteKeys?.prevKey
+                    prevKey
+                        ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
                 }
                 LoadType.APPEND -> {
-                    // You must explicitly check if the last item is null when
-                    // appending, since passing null to networkService is only
-                    // valid for initial load. If lastItem is null it means no
-                    // items were loaded after the initial REFRESH and there are
-                    // no more items to load.
-                    state.pages.last().prevKey
-                    state.lastItemOrNull()
-                        ?: return MediatorResult.Success(
-                            endOfPaginationReached = true
-                        )
-                    Log.e("TAGG", "load: APPEND :${state.pages.last().toString()}")
-                    state.pages.last().prevKey?.plus(1)
+                    val remoteKeys = getRemoteKeyForLastItem(state)
+                    val nextKey = remoteKeys?.nextKey
+                    nextKey
+                        ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
                 }
             }
 
-            val beersList = getBeersUseCase(
-                page = nextPageToLoad ?: 1,
-                pageSize = pageSize,
-            )
-            Log.e("TAGG", "BeerMediator response: $beersList")
-            Log.e("TAGG", "BeerMediator loadKey: $nextPageToLoad")
+            val beerList = getBeersUseCase(page = page, pageSize = state.config.pageSize)
 
-            if (loadType == LoadType.REFRESH) {
-                clearBeersDBUseCase()
+            val endOfPaginationReached = beerList.isEmpty()
+
+            executeDatabaseTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    clearBeersDBUseCase()
+                    clearRemoteKeysUseCase()
+                }
+                val prevKey = if (page > 1) page - 1 else null
+                val nextKey = if (endOfPaginationReached) null else page + 1
+                val remoteKeys = beerList.map {
+                    RemoteKeys(
+                        beerId = it.id,
+                        prevKey = prevKey,
+                        currentPage = page,
+                        nextKey = nextKey
+                    )
+                }
+
+                insertAllRemoteKeysUseCase(remoteKeys)
+                insertBeersIntoDBUseCase(beerList)
             }
-            insertBeersIntoDBUseCase(beersList)
-
-            MediatorResult.Success(
-                endOfPaginationReached = beersList.isEmpty()
-            )
+            return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
         } catch (e: IOException) {
             MediatorResult.Error(e)
         } catch (e: HttpException) {
             MediatorResult.Error(e)
+        }
+    }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, BeerListItem>): RemoteKeys? {
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.id?.let { id ->
+                getRemoteKeyBeBeerIdUseCase(id)
+            }
+        }
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, BeerListItem>): RemoteKeys? {
+        return state.pages.firstOrNull {
+            it.data.isNotEmpty()
+        }?.data?.firstOrNull()?.let { beer ->
+            getRemoteKeyBeBeerIdUseCase(beer.id)
+        }
+    }
+
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, BeerListItem>): RemoteKeys? {
+        return state.pages.lastOrNull {
+            it.data.isNotEmpty()
+        }?.data?.lastOrNull()?.let { beer ->
+            getRemoteKeyBeBeerIdUseCase(beer.id)
         }
     }
 }
