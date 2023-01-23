2b27f1629d538f191135be4fbf02399503c9582e
AlekseyHerman <aliaksei.herman@fingers-media.com>
2023-01-23 12:02:18 +0300

    feat: add test for BeerListScreenViewModel

--- app/src/main/java/com/example/meditationcomposeapp/presentation/screens/main_flow/beer_list/BeerListScreenViewModel.kt
+++ app/src/main/java/com/example/meditationcomposeapp/presentation/screens/main_flow/beer_list/BeerListScreenViewModel.kt
@@ -35,9 +35,6 @@ class BeerListScreenViewModel @Inject constructor(
                     DetailedBeerScreenDestination(
                         beerId
                     )
-//                    DetailedBeerScreenDestination(
-//                        beerId
-//                    )
                 )
             )
         }
