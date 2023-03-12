1282fa3a32d6829458b0fc09e078f89110b485be
Parokq <alekseyherman@gmail.com>
2023-03-12 19:51:38 +0300

    release: version 11 (0.7.2)

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -5,9 +5,9 @@ object Config {
 
     const val MAJOR_VERSION = 0
     const val MINOR_VERSION = 7
-    const val PATCH_VERSION = 1
+    const val PATCH_VERSION = 2
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
-    const val VERSION_CODE = 10
+    const val VERSION_CODE = 11
 
     const val MIN_SDK = 26
     const val TARGET_SDK = 33
--- core/updates_history/src/main/java/com/example/core/updates_history/repository/web/UpdateDescriptionWebRepositoryImpl.kt
+++ core/updates_history/src/main/java/com/example/core/updates_history/repository/web/UpdateDescriptionWebRepositoryImpl.kt
@@ -73,9 +73,9 @@ fun getVersionDescriptions(startFromVersion: String): List<UpdateDescriptionResp
         updateTitle = "Bug fix",
         updateDescription = "Bug with infinite update description pop-up fixed :)",
     )
-    val version0_6_2 = UpdateDescriptionResponse(
+    val version0_7_2 = UpdateDescriptionResponse(
         id = 5,
-        versionName = "0.6.2",
+        versionName = "0.7.2",
         updateReleaseTime = 1678638488581,
         updateTitle = "Bug fixes",
         updateDescription = "Overall bug fixes and performance improvements",
@@ -85,7 +85,7 @@ fun getVersionDescriptions(startFromVersion: String): List<UpdateDescriptionResp
         "0.5.0" to version0_5_0,
         "0.6.0" to version0_6_0,
         "0.6.1" to version0_6_1,
-        "0.6.2" to version0_6_2,
+        "0.7.2" to version0_7_2,
     )
 
     val list = map.values.toTypedArray().copyOfRange(
