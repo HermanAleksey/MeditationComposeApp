cd3a6ad354ee8bd07c4ff8e374263a07266208fd
Parokq <alekseyherman@gmail.com>
2023-04-02 20:39:29 +0300

    feat: add error color

--- core/design_system/src/main/java/com/example/design_system/AppTheme.kt
+++ core/design_system/src/main/java/com/example/design_system/AppTheme.kt
@@ -2,21 +2,14 @@ package com.example.design_system
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 //source for material themes
 //https://material.io/design/color/the-color-system.html#color-theme-creation
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
     //main brand color (app bar uses it)
-    primary = Purple500,
+//    primary = Purple500,
     //used to provide accents
     secondary = Green300,
     //screen background uses it
@@ -24,29 +17,18 @@ private val LightColorPalette = lightColors(
     onSurface = Green700,
     //app background uses it
     background = Green700,
-    onBackground = ColorPalette
-
-
-    //Material also defines "on" colors—colors to use for content on top of one of the named colors
-    // e.g. text in a ‘surface' colored container should be colored ‘on surface'.
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onBackground = ColorPalette,
+    error = AntiqueRuby,
+    onError = ColorPalette
 )
 
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
--- core/design_system/src/main/java/com/example/design_system/Color.kt
+++ core/design_system/src/main/java/com/example/design_system/Color.kt
@@ -2,19 +2,15 @@ package com.example.design_system
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val Green300 = Color(0xFF7C9A92)
 val Green700 = Color(0xFF253334)
 
 val ColorPlatinum = Color(0xFFE5E5E5)
 val ColorPalette = Color(0xFFF7F3F0)
 
-val ColorLoadingProgress = Color(0xFFBEC2C2)
+val SilverSand = Color(0xFFBEC2C2)
 
+val AntiqueRuby = Color(0xFF812525)
 
 //val Colors.colorBackground: Color
 //    @Composable
