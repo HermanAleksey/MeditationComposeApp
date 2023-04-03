2ad27353834fece9e2602f377c74910305087249
Parokq <alekseyherman@gmail.com>
2023-04-03 09:36:50 +0300

    feat: rework SplashScreen; add preview

--- feature/splash_screen/src/main/java/com/example/splash_screen/internal/InternalSplashScreen.kt
+++ feature/splash_screen/src/main/java/com/example/splash_screen/internal/InternalSplashScreen.kt
@@ -2,20 +2,28 @@ package com.example.splash_screen.internal
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.design_system.AppTheme
 import com.example.design_system.common_composables.ImageBackground
 import com.example.feature.splash_screen.R
-import com.example.splash_screen.api.SplashScreenViewModel
 
 @Composable
 internal fun InternalSplashScreen(
-    viewModel: SplashScreenViewModel,
-    currentVersionName: String
+    onLaunch: () -> Unit,
 ) {
     LaunchedEffect(key1 = Unit) {
-        viewModel.onLaunchSplashScreen(currentVersionName)
+        onLaunch()
     }
 
     ImageBackground(
         imageRes = R.drawable.background_login
     ) {}
+}
+
+@Preview
+@Composable
+fun InternalSplashScreenPreview() {
+    AppTheme {
+        InternalSplashScreen {}
+    }
 }
