9df320ef43de9c5b747e5fdacaafb46f35cf3ecd
Parokq <alekseyherman@gmail.com>
2023-04-03 10:53:21 +0300

    feat: rework BeerSorts screens; add preview

--- feature/beer_sorts/src/main/java/com/example/beer_sorts/api/beer_list/BeerListScreen.kt
+++ feature/beer_sorts/src/main/java/com/example/beer_sorts/api/beer_list/BeerListScreen.kt
@@ -1,11 +1,17 @@
 package com.example.beer_sorts.api.beer_list
 
 import androidx.compose.runtime.Composable
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.beer_sorts.internal.presentation.beer_list.InternalBeerListScreen
 
 @Composable
 fun BeerListScreen(
     viewModel: BeerListScreenViewModel
 ) {
-    InternalBeerListScreen(viewModel)
+    val beersPaging = viewModel.beersPagingFlow.collectAsLazyPagingItems()
+
+    InternalBeerListScreen(
+        beersPaging = beersPaging,
+        onBeerItemClicked = viewModel::onBeerItemClicked
+    )
 }
