74fd86fcb4520eba00bf6d0c9f9c542335e339b3
Parokq <alekseyherman@gmail.com>
2023-04-03 09:17:09 +0300

    feat: add common module dep into ShufflePuzzle

--- feature/shuffle_puzzle/build.gradle.kts
+++ feature/shuffle_puzzle/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
     implementation(project(":core:design_system"))
+    implementation(project(":core:common"))
 
     implementation(Dependencies.activity_compose)
     implementation(Dependencies.compose_ui)
