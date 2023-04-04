fa615ffa472f3123ba34cf06369c1aa0b27a750a
Parokq <alekseyherman@gmail.com>
2023-04-04 16:56:30 +0300

    feat: init module

---
+++ feature/internet_connection/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.internet_connection"
+    compileSdk = Config.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Config.MIN_SDK
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    @Suppress("UnstableApiUsage")
+    buildFeatures {
+        compose = true
+    }
+    @Suppress("UnstableApiUsage")
+    composeOptions {
+        kotlinCompilerExtensionVersion = LibVersions.compose_compiler_version
+    }
+}
+
+dependencies {
+    implementation(project(":core:design_system"))
+
+    implementation(Dependencies.activity_compose)
+    implementation(Dependencies.compose_ui)
+    implementation(Dependencies.compose_material)
+    implementation(Dependencies.compose_ui_tooling_preview)
+    implementation(Dependencies.compose_ui_tooling)
+}
