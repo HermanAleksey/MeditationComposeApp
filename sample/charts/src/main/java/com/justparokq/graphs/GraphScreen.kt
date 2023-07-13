588c1422a29569bab0274cbf948ba34696f8ef62
Parokq <alekseyherman@gmail.com>
2023-07-13 21:25:38 +0300

    feat: create entry point

--- sample/charts/src/main/java/com/justparokq/graphs/GraphScreen.kt
+++ sample/charts/src/main/java/com/justparokq/graphs/GraphScreen.kt
@@ -3,9 +3,10 @@ package com.justparokq.graphs
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,64 +15,81 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.tehras.charts.bar.BarChart
 import com.justparokq.graphs.lib.pie_chart.PieChart
 import com.justparokq.graphs.lib.pie_chart.PieChartData
 import com.justparokq.graphs.test_drawers.BarChartDataModel
+import com.justparokq.graphs.test_drawers.getTestPieChartData
+
+enum class GraphType {
+    PIE, BAR, LINE
+}
 
 @Composable
 internal fun GraphScreen() {
 
-    var selectedSlice: PieChartData.Slice? by remember {
+    var showedChart: GraphType? by remember {
         mutableStateOf(null)
     }
 
     Column(
         verticalArrangement = Arrangement.Top,
-        modifier = Modifier
+        modifier = Modifier.fillMaxSize()
     ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { showedChart = GraphType.PIE }) {
+                Text(text = "Плоская круговая", fontSize = 14.sp)
+            }
+            Button(onClick = { showedChart = GraphType.BAR }) {
+                Text(text = "Гистограмма", fontSize = 14.sp)
+            }
+        }
         Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.5f)
+                .fillMaxSize()
+                .padding(vertical = 64.dp, horizontal = 12.dp)
         ) {
-            val barChartDataModel = BarChartDataModel()
-            BarChart(
-                barChartData = barChartDataModel.barChartData,
-                labelDrawer = barChartDataModel.labelDrawer
-            )
-        }
-
-        PieChart(
-            pieChartData = PieChartData(
-                slices = listOf(
-                    PieChartData.Slice(20f, Color.Red),
-                    PieChartData.Slice(30f, Color.Blue),
-                    PieChartData.Slice(50f, Color.Green)
-                )
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            selectedSlice = selectedSlice,
-            onSelectedSliceChanged = { newSlice ->
-                selectedSlice = if (newSlice == selectedSlice) {
-                    null
-                } else {
-                    newSlice
+            when (showedChart) {
+                GraphType.PIE -> {
+                    PieChartPanel()
                 }
+                GraphType.BAR -> {
+                    BarChartPanel()
+                }
+                GraphType.LINE -> {}
+                null -> {}
             }
-        )
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Плоская круговая")
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Гистограмма")
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "С маркерами")
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Динамический")
         }
     }
+}
+
+@Composable
+fun BarChartPanel() {
+    val barChartDataModel = BarChartDataModel()
+    BarChart(
+        barChartData = barChartDataModel.barChartData,
+        labelDrawer = barChartDataModel.labelDrawer
+    )
+}
+
+@Composable
+fun PieChartPanel() {
+    var selectedSlice: PieChartData.Slice? by remember {
+        mutableStateOf(null)
+    }
+
+    PieChart(
+        pieChartData = getTestPieChartData(),
+        modifier = Modifier.fillMaxWidth(),
+        selectedSlice = selectedSlice,
+        onSelectedSliceChanged = { newSlice ->
+            selectedSlice = if (newSlice == selectedSlice) {
+                null
+            } else {
+                newSlice
+            }
+        }
+    )
 }
