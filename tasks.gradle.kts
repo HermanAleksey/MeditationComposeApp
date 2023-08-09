65b214dd56463ecdba2b932aa84ac6cb4500cc4c
Parokq <alekseyherman@gmail.com>
2023-08-09 11:11:45 +0300

    feature: create gradle task that can create modules from template

---
+++ tasks.gradle.kts
@@ -0,0 +1,195 @@
+enum class ModuleType(val path: String) {
+    FEATURE("feature"), CORE("core"), SAMPLE("sample")
+}
+
+fun copyTemplate(fileFrom: File, fileTo: File) {
+    val contentBytes = fileFrom.let {
+        it.createNewFile()
+        it.readBytes()
+    }
+
+    fileTo.apply {
+        createNewFile()
+        writeBytes(contentBytes)
+    }
+}
+
+tasks {
+    val type: String by project //type of module
+    val nName: String by project //module name
+    val tests: String? by project //create test folder?
+    val screenName: String? by project //create test folder?
+
+    task("createModule") {
+        doFirst {
+            if (!project.hasProperty("type")) {
+                println("Not provided -Ptype. It should contain project type: feature, core, sample")
+                return@doFirst
+            }
+            if (!project.hasProperty("nName")) {
+                println("Not provided -PnName. It should contain module name.")
+                return@doFirst
+            }
+            val type = when (type) {
+                "feature" -> ModuleType.FEATURE
+                "core" -> ModuleType.CORE
+                "sample" -> ModuleType.SAMPLE
+                else -> {
+                    println("Param -Ptype is not valid, or does not exist")
+                    return@doFirst
+                }
+            }
+            val createTestFolder = tests != null
+            val createFeatureModuleClasses = type == ModuleType.FEATURE && !screenName.isNullOrEmpty()
+            val modulePath = "${type.path}/$nName/src/main/java/com.justparokq.$nName"
+            val namespace = "com.justparokq.${type.path}.$nName"
+
+            logTaskParams(type.path, nName, createTestFolder, screenName)
+
+            createBaseModuleClasses(
+                moduleType = type,
+                modulePath = modulePath,
+                namespace = namespace,
+                featureName = nName,
+                createTestFolder = createTestFolder,
+            )
+
+            if (createFeatureModuleClasses)
+                createMviClasses(
+                    namespace = namespace,
+                    modulePath = modulePath,
+                    screenName = screenName!!
+                )
+        }
+    }
+}
+
+fun logTaskParams(type: String, nName: String, createTestFolder: Boolean, screenName: String?) {
+    println(
+        """
+                -------------------------------------------
+                Creating module.
+                type = $type
+                module name = $nName
+                createTestDir = $createTestFolder
+                
+                create mvi screen = ${!screenName.isNullOrEmpty()} 
+                ${if (!screenName.isNullOrEmpty()) "mvi screen name= ${screenName!!}" else "Not specified"}  
+                -------------------------------------------
+                """.trimIndent()
+    )
+}
+
+fun createBaseModuleClasses(
+    moduleType: ModuleType,
+    modulePath: String,
+    namespace: String,
+    featureName: String,
+    createTestFolder: Boolean
+) {
+    file(modulePath).mkdirs()
+
+    val screenshotsDirectory = "${moduleType.path}/$featureName/screenshots"
+    file(screenshotsDirectory).mkdirs()
+
+    val gitIgnoreFile = "${moduleType.path}/$featureName/.gitignore"
+    with(file(gitIgnoreFile)) {
+        createNewFile()
+        val manifestTemplate = file("$TEMPLATES_DIR_PATH/.gitignore")
+        copyTemplate(manifestTemplate, this)
+    }
+
+    val buildGradleFile = "${moduleType.path}/$featureName/build.gradle.kts"
+    with(file(buildGradleFile)) {
+        createNewFile()
+        val manifestTemplate = file("$TEMPLATES_DIR_PATH/build.gradle.kts")
+        copyTemplate(manifestTemplate, this)
+
+        readText().replace("#{NAMESPACE}#", namespace).let {
+            writeText(it)
+        }
+    }
+
+    val manifestFile = "${moduleType.path}/$featureName/src/main/AndroidManifest.xml"
+    with(file(manifestFile)) {
+        createNewFile()
+        val manifestTemplate = file("$TEMPLATES_DIR_PATH/AndroidManifest.xml")
+        copyTemplate(manifestTemplate, this)
+    }
+    
+    with(file("settings.gradle.kts")) {
+        val content = this.readText()
+        val includeString = "include(\":${moduleType.path}:$featureName\")"
+        writeText("$content\n$includeString")
+    }
+
+    if (createTestFolder) {
+        val testModulePath = "${moduleType.path}/$featureName/src/test/java/com/justparokq/$featureName"
+        file(testModulePath).mkdirs()
+    }
+}
+
+val TEMPLATES_DIR_PATH = "template"
+val TEMPLATES_MVI_DIR_PATH = "template/mvi"
+
+fun createMviClasses(namespace: String, modulePath: String, screenName: String) {
+    val apiDirectoryPath = "$modulePath/api"
+    val internalDirectoryPath = "$modulePath/internal"
+    val importPath = "$namespace."
+    file(apiDirectoryPath).mkdirs()
+    file(internalDirectoryPath).mkdirs()
+
+    createFilesFromTemplate(
+        newFilePath = apiDirectoryPath + "/${screenName}.kt",
+        templatePath = "$TEMPLATES_MVI_DIR_PATH/ScreenTemplate",
+        screenName = screenName,
+        namespace = namespace,
+    )
+
+    createFilesFromTemplate(
+        newFilePath = apiDirectoryPath + "/${screenName}Action.kt",
+        templatePath = "$TEMPLATES_MVI_DIR_PATH/ActionTemplate",
+        screenName = screenName,
+        namespace = namespace,
+    )
+
+    createFilesFromTemplate(
+        newFilePath = apiDirectoryPath + "/${screenName}ViewModel.kt",
+        templatePath = "$TEMPLATES_MVI_DIR_PATH/ViewModelTemplate",
+        screenName = screenName,
+        namespace = namespace,
+    )
+
+    createFilesFromTemplate(
+        newFilePath = apiDirectoryPath + "/${screenName}NavDependenciesTemplate.kt",
+        templatePath = "$TEMPLATES_MVI_DIR_PATH/NavDependenciesTemplate",
+        screenName = screenName,
+        namespace = namespace,
+    )
+
+    createFilesFromTemplate(
+        newFilePath = internalDirectoryPath + "/Internal${screenName}.kt",
+        templatePath = "$TEMPLATES_MVI_DIR_PATH/InternalScreenTemplate",
+        screenName = screenName,
+        namespace = namespace,
+    )
+}
+
+val SCREEN_NAME_MOCK = "#{SCREEN_NAME}#"
+val NAMESPACE_MOCK = "#{NAMESPACE}#"
+
+fun createFilesFromTemplate(newFilePath: String, templatePath: String, screenName: String, namespace: String) {
+    with(file(newFilePath)) {
+        createNewFile()
+        val template = file(templatePath)
+        copyTemplate(template, this)
+
+        readText()
+            .replace(SCREEN_NAME_MOCK, screenName)
+            .replace(NAMESPACE_MOCK, namespace)
+            .let {
+                writeText(it)
+            }
+    }
+}
+
---
+++ template/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.devtools.ksp") version LibVersions.ksp_version
+}
+
+android {
+    namespace = "#{NAMESPACE}#"
+    compileSdk = Config.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Config.MIN_SDK
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    @Suppress("UnstableApiUsage")
+    buildFeatures {
+        compose = true
+    }
+    @Suppress("UnstableApiUsage")
+    composeOptions {
+        kotlinCompilerExtensionVersion = LibVersions.compose_compiler_version
+    }
+}
+
+dependencies {
+    implementation(project(":core:design_system"))
+    implementation(project(":core:common"))
+    implementation(project(":core:data_store"))
+
+    implementation(Dependencies.activity_compose)
+    implementation(Dependencies.compose_ui)
+    implementation(Dependencies.compose_material)
+    implementation(Dependencies.compose_ui_tooling_preview)
+    implementation(Dependencies.compose_ui_tooling)
+
+    //hilt
+    implementation(Dependencies.hilt_android)
+    kapt(Dependencies.hilt_compiler)
+}
